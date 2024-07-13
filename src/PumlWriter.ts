d3dd71d7668e00f40c10be12bf6e6166e3226a7f
ModEraNoah <klauck02@outlook.com>
2024-07-13 13:02:24 +0200

    feat: puml for class metadata & dependencies

--- src/PumlWriter.ts
+++ src/PumlWriter.ts
@@ -10,6 +10,36 @@ export class PumlWriter {
 		console.log("@enduml")
 	}
 
+	public writeClassDependencies(classRelations: ClassRelations): void {
+		if (classRelations.parent) {
+			console.log(`${classRelations.className} --|> ${classRelations.parent}`)
+		}
+
+		for (let i = 0; i < classRelations.dependencies.length; i++) {
+			if (!isNativeType(classRelations.dependencies[i])) {
+				console.log(`${classRelations.className} -> ${classRelations.dependencies[i].replace("[]", "")}`)
+			}
+		}
+	}
+
+	public writeClassMetaData(classContext: ClassContext): void {
+		console.log(`class ${classContext.name} {`)
+
+		classContext.methods.forEach(method => {
+			let parameterString = ""
+			for (let i = 0; i < method.parameters.length; i++) {
+				if (i === 0) {
+					parameterString += `${method.parameters[i].name}: ${method.parameters[i].type}`
+				} else {
+					parameterString += `, ${method.parameters[i].name}: ${method.parameters[i].type}`
+				}
+			}
+			console.log(`${this.translateAccessModifyer(method.accessModifyer)} ${method.name} (${parameterString}): ${method.return}`)
+		})
+
+		console.log("}")
+	}
+
 	private translateAccessModifyer(access: string): string {
 		const relevantModifyer = access.split(" ")[0]
 		switch (relevantModifyer) {
