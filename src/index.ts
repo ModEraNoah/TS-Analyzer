0b1fb409a0fe789893d1f6145a11de29119e5ed2
ModEraNoah <klauck02@outlook.com>
2024-07-13 11:52:52 +0200

    chore: added relation solver & output

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { Main } from "./Main";
 import { readFile } from "./Reader";
+import { RelationSolver } from "./RelationSolver";
 import { Tokenizer } from "./Tokenizer";
 
-const path = "/home/noah/Documents/prog/TS-Analyzer/src/Tokenizer.ts"
+// const path = "/home/noah/Documents/prog/TS-Analyzer/src/Tokenizer.ts"
+const path = "/home/noah/Documents/prog/TS-Analyzer/src/Main.ts"
 const content = readFile(path)
 console.log("content-lengt:", content.length)
 console.log("")
@@ -12,3 +14,6 @@ const main = new Main("Main.ts", content, tokenizer)
 main.main()
 console.log("====================\n Main-Context:\n==================")
 console.log(main["context"])
+
+const relationSolver = new RelationSolver()
+console.log(relationSolver.getClassDependencies(main["context"].classes))
