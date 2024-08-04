b8dee6d0be131cc220565b1aa3719f1c32459bb8
ModEraNoah <klauck02@outlook.com>
2024-08-04 14:31:35 +0200

    test: ClassToken unittest

---
+++ test/token/ClassToken.test.ts
@@ -0,0 +1,35 @@
+import { MainContext } from "../../src/Context"
+import { ClassToken } from "../../src/token/ClassToken"
+
+describe("ClassToken", () => {
+	describe("getTokenEnd", () => {
+		//
+	})
+
+	describe("processToken", () => {
+		test("class without parent", () => {
+			const context: MainContext = { context: "main", fileName: "filename", functions: [], classes: [], variables: [] }
+			const content = "class SomeTestClass {xxx}"
+
+			const classToken = new ClassToken(0)
+			classToken.processToken(context.classes, content)
+			expect(context.classes).toEqual([{ context: "class", name: "SomeTestClass", parent: "", attributes: [], methods: [] }])
+		})
+		test("class implements interface", () => {
+			const context: MainContext = { context: "main", fileName: "filename", functions: [], classes: [], variables: [] }
+			const content = "class SomeTestClass implements ISomeClass {xxx}"
+
+			const classToken = new ClassToken(0)
+			classToken.processToken(context.classes, content)
+			expect(context.classes).toEqual([{ context: "class", name: "SomeTestClass", parent: "ISomeClass", attributes: [], methods: [] }])
+		})
+		test("class extends parent", () => {
+			const context: MainContext = { context: "main", fileName: "filename", functions: [], classes: [], variables: [] }
+			const content = "class SomeTestClass extends ISomeClass {xxx}"
+
+			const classToken = new ClassToken(0)
+			classToken.processToken(context.classes, content)
+			expect(context.classes).toEqual([{ context: "class", name: "SomeTestClass", parent: "ISomeClass", attributes: [], methods: [] }])
+		})
+	})
+})
