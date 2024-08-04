6601c83792c2af82a4cd562212980127eb638848
ModEraNoah <klauck02@outlook.com>
2024-08-04 13:59:31 +0200

    test: unittest util getFunctionMetaData

--- test/util.test.ts
+++ test/util.test.ts
@@ -1,4 +1,4 @@
-import { getClosingBracketIndex } from "../src/util"
+import { getClosingBracketIndex, getFunctionMetaData } from "../src/util"
 
 describe("util functionalities", () => {
 	describe("getClosingBracketIndex", () => {
@@ -12,6 +12,63 @@ describe("util functionalities", () => {
 
 		test("closing bracket not in content", () => {
 			expect(getClosingBracketIndex(0, "{some Content string")).toBe(-1)
+		})
+	})
+
+	describe("getFunctionMetaData", () => {
+		test("no params", () => {
+			const content = "function someFunc() { console.log('test'); }"
+			const startIdx = 0
+
+			expect(getFunctionMetaData(content, startIdx)).toEqual({ parameters: [], returnType: "any", paramsOpeningBracketIdx: 17 })
+		})
+		test("one param", () => {
+			const content = "function someFunc(someParam: string) { console.log('test'); }"
+			const startIdx = 0
+
+			expect(getFunctionMetaData(content, startIdx)).toEqual({ parameters: [{ context: "variable", name: "someParam", type: "string" }], returnType: "any", paramsOpeningBracketIdx: 17 })
+		})
+		test("multiple params", () => {
+			const content = "function someFunc(someParam: string, other: number) { console.log('test'); }"
+			const startIdx = 0
+
+			expect(getFunctionMetaData(content, startIdx)).toEqual({ parameters: [{ context: "variable", name: "someParam", type: "string" }, { context: "variable", name: "other", type: "number" }], returnType: "any", paramsOpeningBracketIdx: 17 })
+		})
+		test("object as param", () => {
+			const content = "function someFunc(someParam: {key: string}) { console.log('test'); }"
+			const startIdx = 0
+
+			expect(getFunctionMetaData(content, startIdx)).toEqual({ parameters: [{ context: "variable", name: "someParam", type: "{key: string}" }], returnType: "any", paramsOpeningBracketIdx: 17 })
+		})
+		test("array as param", () => {
+			const content = "function someFunc(someParam: string[]) { console.log('test'); }"
+			const startIdx = 0
+
+			expect(getFunctionMetaData(content, startIdx)).toEqual({ parameters: [{ context: "variable", name: "someParam", type: "string[]" }], returnType: "any", paramsOpeningBracketIdx: 17 })
+		})
+		test("object as return", () => {
+			const content = "function someFunc(someParam: string): {key: string} { console.log('test'); }"
+			const startIdx = 0
+
+			expect(getFunctionMetaData(content, startIdx)).toEqual({ parameters: [{ context: "variable", name: "someParam", type: "string" }], returnType: "{key: string}", paramsOpeningBracketIdx: 17 })
+		})
+		test("array as return", () => {
+			const content = "function someFunc(someParam: string): string[] { console.log('test'); }"
+			const startIdx = 0
+
+			expect(getFunctionMetaData(content, startIdx)).toEqual({ parameters: [{ context: "variable", name: "someParam", type: "string" }], returnType: "string[]", paramsOpeningBracketIdx: 17 })
+		})
+		test("no return", () => {
+			const content = "function someFunc(someParam: string): any { console.log('test'); }"
+			const startIdx = 0
+
+			expect(getFunctionMetaData(content, startIdx)).toEqual({ parameters: [{ context: "variable", name: "someParam", type: "string" }], returnType: "any", paramsOpeningBracketIdx: 17 })
+		})
+		test("native return", () => {
+			const content = "function someFunc(someParam: string): number { console.log('test'); }"
+			const startIdx = 0
+
+			expect(getFunctionMetaData(content, startIdx)).toEqual({ parameters: [{ context: "variable", name: "someParam", type: "string" }], returnType: "number", paramsOpeningBracketIdx: 17 })
 		})
 	})
 })
