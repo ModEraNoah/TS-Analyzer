72db65a9cddc0c50f276dd06049a0c3b83d0f03a
ModEraNoah <klauck02@outlook.com>
2024-08-04 14:32:12 +0200

    test: AccessModifyerToken unittest

---
+++ test/token/AccessModifyer.test.ts
@@ -0,0 +1,39 @@
+import { AccessModifyerToken } from "../../src/token/AccessModifyerToken"
+
+describe("AccessModifyer", () => {
+	describe("specifyType", () => {
+		test("method", () => {
+			const content = "public someMethod(): string { console.log ('test') }"
+			const token = new AccessModifyerToken(0)
+			token["specifyType"](content)
+			expect(token["type"]).toEqual("method")
+		})
+
+		describe("attribute", () => {
+			test("without initiation & without type", () => {
+				const content = "public params;"
+				const token = new AccessModifyerToken(0)
+				token["specifyType"](content)
+				expect(token["type"]).toEqual("attribute")
+			})
+			test("without initation & with type", () => {
+				const content = "public params: string;"
+				const token = new AccessModifyerToken(0)
+				token["specifyType"](content)
+				expect(token["type"]).toEqual("attribute")
+			})
+			test("with initiation & without type", () => {
+				const content = "public params = 3"
+				const token = new AccessModifyerToken(0)
+				token["specifyType"](content)
+				expect(token["type"]).toEqual("attribute")
+			})
+			test("with initation & with type", () => {
+				const content = "public params: string = 'hallo'"
+				const token = new AccessModifyerToken(0)
+				token["specifyType"](content)
+				expect(token["type"]).toEqual("attribute")
+			})
+		})
+	})
+})
