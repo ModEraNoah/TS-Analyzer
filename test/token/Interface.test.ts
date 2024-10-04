3947484855b551246eab0b6333c3c9a8dae1b3c7
ModEraNoah <klauck02@outlook.com>
2024-10-04 12:14:59 +0200

    test: adapting to methods support for interfaces

--- test/token/Interface.test.ts
+++ test/token/Interface.test.ts
@@ -1,168 +1,198 @@
 import { InterfaceToken } from "../../src/token/InterfaceToken";
 
 describe("InterfaceToken", () => {
-	describe("getInterfaceAttributes", () => {
+	describe("getInterfaceAttributesAndMethods", () => {
 		const token = new InterfaceToken(0);
 		test("one attribute", () => {
 			const interfaceContent = ["key: string"];
-			expect(token["getInterfaceAttributes"](interfaceContent)).toEqual([
-				{
-					context: "variable",
-					name: "key",
-					type: "string",
-					accessModifyer: "public",
-				},
-			]);
+			expect(token["getInterfaceAttributesAndMethods"](interfaceContent)).toEqual({
+				attributes: [
+					{
+						context: "variable",
+						name: "key",
+						type: "string",
+						accessModifyer: "public",
+					},
+				],
+				methods: [],
+			});
 		});
 
 		test("two attributes", () => {
 			const interfaceContent = ["key: string", "other: number"];
-			expect(token["getInterfaceAttributes"](interfaceContent)).toEqual([
-				{
-					context: "variable",
-					name: "key",
-					type: "string",
-					accessModifyer: "public",
-				},
-				{
-					context: "variable",
-					name: "other",
-					type: "number",
-					accessModifyer: "public",
-				},
-			]);
+			expect(token["getInterfaceAttributesAndMethods"](interfaceContent)).toEqual({
+				attributes: [
+					{
+						context: "variable",
+						name: "key",
+						type: "string",
+						accessModifyer: "public",
+					},
+					{
+						context: "variable",
+						name: "other",
+						type: "number",
+						accessModifyer: "public",
+					},
+				],
+				methods: [],
+			});
 		});
 
 		test("three attributes", () => {
 			const interfaceContent = ["key: string", "other: number", "thirdKey: boolean"];
-			expect(token["getInterfaceAttributes"](interfaceContent)).toEqual([
-				{
-					context: "variable",
-					name: "key",
-					type: "string",
-					accessModifyer: "public",
-				},
-				{
-					context: "variable",
-					name: "other",
-					type: "number",
-					accessModifyer: "public",
-				},
-				{
-					context: "variable",
-					name: "thirdKey",
-					type: "boolean",
-					accessModifyer: "public",
-				},
-			]);
+			expect(token["getInterfaceAttributesAndMethods"](interfaceContent)).toEqual({
+				attributes: [
+					{
+						context: "variable",
+						name: "key",
+						type: "string",
+						accessModifyer: "public",
+					},
+					{
+						context: "variable",
+						name: "other",
+						type: "number",
+						accessModifyer: "public",
+					},
+					{
+						context: "variable",
+						name: "thirdKey",
+						type: "boolean",
+						accessModifyer: "public",
+					},
+				],
+				methods: [],
+			});
 		});
 
 		test("attribute of type array", () => {
 			const interfaceContent = ["key: string[]"];
-			expect(token["getInterfaceAttributes"](interfaceContent)).toEqual([
-				{
-					context: "variable",
-					name: "key",
-					type: "string[]",
-					accessModifyer: "public",
-				},
-			]);
+			expect(token["getInterfaceAttributesAndMethods"](interfaceContent)).toEqual({
+				attributes: [
+					{
+						context: "variable",
+						name: "key",
+						type: "string[]",
+						accessModifyer: "public",
+					},
+				],
+				methods: [],
+			});
 		});
 
 		test("attribute of type object with one key", () => {
 			const interfaceContent = ["key: {some: number}"];
-			expect(token["getInterfaceAttributes"](interfaceContent)).toEqual([
-				{
-					context: "variable",
-					name: "key",
-					type: "{some: number}",
-					accessModifyer: "public",
-				},
-			]);
+			expect(token["getInterfaceAttributesAndMethods"](interfaceContent)).toEqual({
+				attributes: [
+					{
+						context: "variable",
+						name: "key",
+						type: "{some: number}",
+						accessModifyer: "public",
+					},
+				],
+				methods: [],
+			});
 		});
 
 		test("attribute of type object with multiple keys", () => {
 			const interfaceContent = ["key: {some: number, other: boolean, nested: {k: number}}"];
-			expect(token["getInterfaceAttributes"](interfaceContent)).toEqual([
-				{
-					context: "variable",
-					name: "key",
-					type: "{some: number, other: boolean, nested: {k: number}}",
-					accessModifyer: "public",
-				},
-			]);
+			expect(token["getInterfaceAttributesAndMethods"](interfaceContent)).toEqual({
+				attributes: [
+					{
+						context: "variable",
+						name: "key",
+						type: "{some: number, other: boolean, nested: {k: number}}",
+						accessModifyer: "public",
+					},
+				],
+				methods: [],
+			});
 		});
 
-		test("one function", () => {
+		test("one anonymous function", () => {
 			const interfaceContent = ["key: () => number"];
-			expect(token["getInterfaceAttributes"](interfaceContent)).toEqual([
-				{
-					context: "variable",
-					name: "key",
-					type: "() => number",
-					accessModifyer: "public",
-				},
-			]);
+			expect(token["getInterfaceAttributesAndMethods"](interfaceContent)).toEqual({
+				attributes: [
+					{
+						context: "variable",
+						name: "key",
+						type: "() => number",
+						accessModifyer: "public",
+					},
+				],
+				methods: [],
+			});
 		});
 
-		test("two functions", () => {
+		test("two anonymous functions", () => {
 			const interfaceContent = ["key: () => number", "otherFunc: () => {someType: string}"];
-			expect(token["getInterfaceAttributes"](interfaceContent)).toEqual([
-				{
-					context: "variable",
-					name: "key",
-					type: "() => number",
-					accessModifyer: "public",
-				},
-				{
-					context: "variable",
-					name: "otherFunc",
-					type: "() => {someType: string}",
-					accessModifyer: "public",
-				},
-			]);
+			expect(token["getInterfaceAttributesAndMethods"](interfaceContent)).toEqual({
+				attributes: [
+					{
+						context: "variable",
+						name: "key",
+						type: "() => number",
+						accessModifyer: "public",
+					},
+					{
+						context: "variable",
+						name: "otherFunc",
+						type: "() => {someType: string}",
+						accessModifyer: "public",
+					},
+				],
+				methods: [],
+			});
 		});
 
-		test("three functions", () => {
+		test("three anonymous functions", () => {
 			const interfaceContent = ["key: () => number", "otherFunc: () => {someType: string}", "foo: () => void"];
-			expect(token["getInterfaceAttributes"](interfaceContent)).toEqual([
-				{
-					context: "variable",
-					name: "key",
-					type: "() => number",
-					accessModifyer: "public",
-				},
-				{
-					context: "variable",
-					name: "otherFunc",
-					type: "() => {someType: string}",
-					accessModifyer: "public",
-				},
-				{
-					context: "variable",
-					name: "foo",
-					type: "() => void",
-					accessModifyer: "public",
-				},
-			]);
+			expect(token["getInterfaceAttributesAndMethods"](interfaceContent)).toEqual({
+				attributes: [
+					{
+						context: "variable",
+						name: "key",
+						type: "() => number",
+						accessModifyer: "public",
+					},
+					{
+						context: "variable",
+						name: "otherFunc",
+						type: "() => {someType: string}",
+						accessModifyer: "public",
+					},
+					{
+						context: "variable",
+						name: "foo",
+						type: "() => void",
+						accessModifyer: "public",
+					},
+				],
+				methods: [],
+			});
 		});
 
-		test("attribute and function", () => {
+		test("attribute and anonymous function", () => {
 			const interfaceContent = ["name: string", "key: () => number"];
-			expect(token["getInterfaceAttributes"](interfaceContent)).toEqual([
-				{
-					context: "variable",
-					name: "name",
-					type: "string",
-					accessModifyer: "public",
-				},
-				{
-					context: "variable",
-					name: "key",
-					type: "() => number",
-					accessModifyer: "public",
-				},
-			]);
+			expect(token["getInterfaceAttributesAndMethods"](interfaceContent)).toEqual({
+				attributes: [
+					{
+						context: "variable",
+						name: "name",
+						type: "string",
+						accessModifyer: "public",
+					},
+					{
+						context: "variable",
+						name: "key",
+						type: "() => number",
+						accessModifyer: "public",
+					},
+				],
+				methods: [],
+			});
 		});
 	});
 });
